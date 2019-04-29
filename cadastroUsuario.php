2902024787267190996e18d55ceb09c535b9ca8b
Romulo Soares <37163123+RomuloLASS@users.noreply.github.com>
2019-04-28 21:34:33 -0300

    Permição por usuario adicionado(Correções)

--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -126,7 +126,7 @@ include "header.php";
 
                 <?php
                 // A sessão precisa ser iniciada em cada página diferente
-
+                if(!isset($_SESSION)) session_start();
                 // Verifica se não há a variável da sessão que identifica o usuário
                 
                 if ( $_SESSION['usuarioNiveisAcessoId'] == 0) {
