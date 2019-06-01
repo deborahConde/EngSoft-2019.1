6e785aeb07e64a5960086aaaf5c5b26979640f7b
Romulo Soares <37163123+RomuloLASS@users.noreply.github.com>
2019-06-01 16:20:16 -0300

    Ateração no BD na tabela de funcionários: Primirary key: id, cpf unique

--- Banco de Dados/lojaze.sql
+++ Banco de Dados/lojaze.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: 19-Maio-2019 às 17:18
--- Versão do servidor: 5.7.24
--- versão do PHP: 7.2.14
+-- Host: 127.0.0.1
+-- Generation Time: 01-Jun-2019 às 21:18
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,8 +28,7 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `clientes`
 --
 
-DROP TABLE IF EXISTS `clientes`;
-CREATE TABLE IF NOT EXISTS `clientes` (
+CREATE TABLE `clientes` (
   `email` varchar(50) NOT NULL,
   `senha` varchar(40) NOT NULL,
   `nome` varchar(40) CHARACTER SET latin1 NOT NULL,
@@ -50,14 +49,20 @@ CREATE TABLE IF NOT EXISTS `clientes` (
 -- Estrutura da tabela `funcionarios`
 --
 
-DROP TABLE IF EXISTS `funcionarios`;
-CREATE TABLE IF NOT EXISTS `funcionarios` (
+CREATE TABLE `funcionarios` (
   `id` varchar(20) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `cpf` varchar(14) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `salario` float NOT NULL,
-  `cargo` varchar(50) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  PRIMARY KEY (`id`,`cpf`) USING BTREE
+  `cargo` varchar(50) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`id`, `cpf`, `salario`, `cargo`) VALUES
+('00000001', '00000000000', 1001, 'Administrador'),
+('36050-220', '02010106660', 250000, 'Vendedor');
 
 -- --------------------------------------------------------
 
@@ -65,8 +70,7 @@ CREATE TABLE IF NOT EXISTS `funcionarios` (
 -- Estrutura da tabela `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE IF NOT EXISTS `usuarios` (
+CREATE TABLE `usuarios` (
   `email` varchar(50) NOT NULL,
   `senha` varchar(40) NOT NULL,
   `nome` varchar(40) CHARACTER SET latin1 NOT NULL,
@@ -77,9 +81,33 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `cidade` varchar(40) CHARACTER SET latin1 NOT NULL,
   `estado` varchar(40) CHARACTER SET latin1 NOT NULL,
   `cep` varchar(10) CHARACTER SET latin1 NOT NULL,
-  `tipo` int(5) NOT NULL,
-  PRIMARY KEY (`cpf`)
+  `tipo` int(5) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`email`, `senha`, `nome`, `telefone`, `cpf`, `endereco`, `complemento`, `cidade`, `estado`, `cep`, `tipo`) VALUES
+('admin@admin', 'admin', 'Admin', '000000000', '00000000000', 'Av.Rio Brando 135, 211, 202', '202', 'Juiz de Fora', 'MG', '00000000', 0),
+('test@test.com', '12345', 'Test Test', '3211111111', '123.456.789-00', 'Av.Rio Brando 1551, 211, 211, apto 100, ', 'apto 100', 'Juiz de Fora', 'Escolha...', '11011-110', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`cpf`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
