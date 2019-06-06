268b3ecaa4171c7a2ac6c3839eac5481641f965e
Gustavo <gustavop.dangelo@gmail.com>
2019-06-06 16:32:34 -0300

    testando correção de dados

---
+++ header.php
@@ -0,0 +1,37 @@
+<!--Menu-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!--Links do Menu-->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="produto.php">Produtos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cadastroUsuario.php">Cadastro de Usuário</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="visualizarUsuario.php">Visualizar Usuário</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="exibirUsuario.php">Exibir Usuário</a>
+      </li>
+      <li>
+        <a class="nav-link" href="visualizarFuncionario.php">Visualizar Funcionário</a>
+      </li>
+      <li>
+        <a class="nav-link" href="visualizarCliente.php">Visualizar Cliente</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+    </ul>
+  </div>
+  <!--Links do Menu-->
+</nav>
+<!--Menu-->
